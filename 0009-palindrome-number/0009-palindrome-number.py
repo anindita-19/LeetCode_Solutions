f966a34cf835193cef366a0ddedd7e53b1c07e6a
Anindita Dutta <anindita.dutta2005@gmail.com>
2026-05-11 11:57:35 +0530

    Time: 21 ms (14.87%), Space: 12.4 MB (18.19%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        num=x
+        s=0
+        while(x>0):
+            d=x%10
+            x=x//10
+            s=s*10+d 
+        if s==num:
+            return True
+        else:
+            return False    
+        
